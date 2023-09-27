a7aee0e9a4602e0dfc8b562434809811e041b2a7
Paulin NZODOUM <PaulUno777@gmail.com>
2023-09-27 21:31:06 +0100

    coool

--- clone-proto.js
+++ clone-proto.js
@@ -1,10 +1,5 @@
 const clone = require('git-clone');
 
-clone(
-  'https://github.com/PaulUno777/kamix-protocol-buffer.git',
-  'proto',
-  { shallow: true },
-  () => {
-    console.log('Repository cloned.');
-  },
-);
+clone('https://github.com/PaulUno777/proto', 'proto', { shallow: true }, () => {
+  console.log('Repository cloned.');
+});
