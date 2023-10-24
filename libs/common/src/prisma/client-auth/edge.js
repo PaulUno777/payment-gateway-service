c3f6b69d2f6c158e1d716a8735e6f72f37634a7a
Paulin NZODOUM <PaulUno777@gmail.com>
2023-10-24 07:30:31 +0100

    update availables providers codes

--- libs/common/src/prisma/client-auth/edge.js
+++ libs/common/src/prisma/client-auth/edge.js
@@ -1,5 +1,4 @@
-
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, '__esModule', { value: true });
 
 const {
   PrismaClientKnownRequestError,
@@ -21,60 +20,57 @@ const {
   warnOnce,
   defineDmmfProperty,
   Public,
-} = require('./runtime/edge')
+} = require('./runtime/edge');
 
+const Prisma = {};
 
-const Prisma = {}
-
-exports.Prisma = Prisma
-exports.$Enums = {}
+exports.Prisma = Prisma;
+exports.$Enums = {};
 
 /**
  * Prisma Client JS version: 5.4.2
  * Query Engine version: ac9d7041ed77bcc8a8dbd2ab6616b39013829574
  */
 Prisma.prismaVersion = {
-  client: "5.4.2",
-  engine: "ac9d7041ed77bcc8a8dbd2ab6616b39013829574"
-}
+  client: '5.4.2',
+  engine: 'ac9d7041ed77bcc8a8dbd2ab6616b39013829574',
+};
 
 Prisma.PrismaClientKnownRequestError = PrismaClientKnownRequestError;
-Prisma.PrismaClientUnknownRequestError = PrismaClientUnknownRequestError
-Prisma.PrismaClientRustPanicError = PrismaClientRustPanicError
-Prisma.PrismaClientInitializationError = PrismaClientInitializationError
-Prisma.PrismaClientValidationError = PrismaClientValidationError
-Prisma.NotFoundError = NotFoundError
-Prisma.Decimal = Decimal
+Prisma.PrismaClientUnknownRequestError = PrismaClientUnknownRequestError;
+Prisma.PrismaClientRustPanicError = PrismaClientRustPanicError;
+Prisma.PrismaClientInitializationError = PrismaClientInitializationError;
+Prisma.PrismaClientValidationError = PrismaClientValidationError;
+Prisma.NotFoundError = NotFoundError;
+Prisma.Decimal = Decimal;
 
 /**
  * Re-export of sql-template-tag
  */
-Prisma.sql = sqltag
-Prisma.empty = empty
-Prisma.join = join
-Prisma.raw = raw
-Prisma.validator = Public.validator
+Prisma.sql = sqltag;
+Prisma.empty = empty;
+Prisma.join = join;
+Prisma.raw = raw;
+Prisma.validator = Public.validator;
 
 /**
-* Extensions
-*/
-Prisma.getExtensionContext = Extensions.getExtensionContext
-Prisma.defineExtension = Extensions.defineExtension
+ * Extensions
+ */
+Prisma.getExtensionContext = Extensions.getExtensionContext;
+Prisma.defineExtension = Extensions.defineExtension;
 
 /**
  * Shorthand utilities for JSON filtering
  */
-Prisma.DbNull = objectEnumValues.instances.DbNull
-Prisma.JsonNull = objectEnumValues.instances.JsonNull
-Prisma.AnyNull = objectEnumValues.instances.AnyNull
+Prisma.DbNull = objectEnumValues.instances.DbNull;
+Prisma.JsonNull = objectEnumValues.instances.JsonNull;
+Prisma.AnyNull = objectEnumValues.instances.AnyNull;
 
 Prisma.NullTypes = {
   DbNull: objectEnumValues.classes.DbNull,
   JsonNull: objectEnumValues.classes.JsonNull,
-  AnyNull: objectEnumValues.classes.AnyNull
-}
-
-
+  AnyNull: objectEnumValues.classes.AnyNull,
+};
 
 /**
  * Enums
@@ -92,24 +88,24 @@ exports.Prisma.UserScalarFieldEnum = {
   phone: 'phone',
   isActive: 'isActive',
   isDeleted: 'isDeleted',
-  role: 'role'
+  role: 'role',
 };
 
 exports.Prisma.RoleScalarFieldEnum = {
   id: 'id',
   createdAt: 'createdAt',
   name: 'name',
-  isDeleted: 'isDeleted'
+  isDeleted: 'isDeleted',
 };
 
 exports.Prisma.SortOrder = {
   asc: 'asc',
-  desc: 'desc'
+  desc: 'desc',
 };
 
 exports.Prisma.QueryMode = {
   default: 'default',
-  insensitive: 'insensitive'
+  insensitive: 'insensitive',
 };
 exports.RoleType = exports.$Enums.RoleType = {
   super_admin: 'super_admin',
@@ -117,81 +113,95 @@ exports.RoleType = exports.$Enums.RoleType = {
   manage_users: 'manage_users',
   all: 'all',
   new_role: 'new_role',
-  api_client: 'api_client'
+  api_client: 'api_client',
 };
 
 exports.Prisma.ModelName = {
   User: 'User',
-  Role: 'Role'
+  Role: 'Role',
 };
 /**
  * Create the Client
  */
 const config = {
-  "generator": {
-    "name": "client",
-    "provider": {
-      "fromEnvVar": null,
-      "value": "prisma-client-js"
+  generator: {
+    name: 'client',
+    provider: {
+      fromEnvVar: null,
+      value: 'prisma-client-js',
     },
-    "output": {
-      "value": "E:\\Coding\\kamix\\payment-gateway-service\\libs\\common\\src\\prisma\\client-auth",
-      "fromEnvVar": null
+    output: {
+      value:
+        'E:\\Coding\\kamix\\payment-gateway-service\\libs\\common\\src\\prisma\\client-auth',
+      fromEnvVar: null,
     },
-    "config": {
-      "engineType": "library"
+    config: {
+      engineType: 'library',
     },
-    "binaryTargets": [
+    binaryTargets: [
       {
-        "fromEnvVar": null,
-        "value": "windows",
-        "native": true
-      }
+        fromEnvVar: null,
+        value: 'windows',
+        native: true,
+      },
     ],
-    "previewFeatures": [],
-    "isCustomOutput": true
+    previewFeatures: [],
+    isCustomOutput: true,
   },
-  "relativeEnvPaths": {
-    "rootEnvPath": "../../../../../.env",
-    "schemaEnvPath": "../../../../../.env"
+  relativeEnvPaths: {
+    rootEnvPath: '../../../../../.env',
+    schemaEnvPath: '../../../../../.env',
   },
-  "relativePath": "../../../../../prisma",
-  "clientVersion": "5.4.2",
-  "engineVersion": "ac9d7041ed77bcc8a8dbd2ab6616b39013829574",
-  "datasourceNames": [
-    "db"
-  ],
-  "activeProvider": "mongodb",
-  "postinstall": false,
-  "inlineDatasources": {
-    "db": {
-      "url": {
-        "fromEnvVar": "AUTH_DATABASE_URL",
-        "value": null
-      }
-    }
+  relativePath: '../../../../../prisma',
+  clientVersion: '5.4.2',
+  engineVersion: 'ac9d7041ed77bcc8a8dbd2ab6616b39013829574',
+  datasourceNames: ['db'],
+  activeProvider: 'mongodb',
+  postinstall: false,
+  inlineDatasources: {
+    db: {
+      url: {
+        fromEnvVar: 'AUTH_DATABASE_URL',
+        value: null,
+      },
+    },
   },
-  "inlineSchema": "Ly8gVGhpcyBpcyB5b3VyIFByaXNtYSBzY2hlbWEgZmlsZSwKLy8gbGVhcm4gbW9yZSBhYm91dCBpdCBpbiB0aGUgZG9jczogaHR0cHM6Ly9wcmlzLmx5L2QvcHJpc21hLXNjaGVtYQoKZ2VuZXJhdG9yIGNsaWVudCB7CiAgcHJvdmlkZXIgPSAicHJpc21hLWNsaWVudC1qcyIKICBvdXRwdXQgICA9ICIuLi9saWJzL2NvbW1vbi9zcmMvcHJpc21hL2NsaWVudC1hdXRoIgp9CgpkYXRhc291cmNlIGRiIHsKICBwcm92aWRlciA9ICJtb25nb2RiIgogIHVybCAgICAgID0gZW52KCJBVVRIX0RBVEFCQVNFX1VSTCIpCn0KCm1vZGVsIFVzZXIgewogIGlkICAgICAgICBTdHJpbmcgICBAaWQgQGRlZmF1bHQoYXV0bygpKSBAbWFwKCJfaWQiKSBAZGIuT2JqZWN0SWQKICBjcmVhdGVkQXQgRGF0ZVRpbWUgQGRlZmF1bHQobm93KCkpIEBtYXAoImNyZWF0ZWRfYXQiKQogIHVwZGF0ZWRBdCBEYXRlVGltZSBAdXBkYXRlZEF0IEBtYXAoInVwZGF0ZWRfYXQiKQoKICB1aWQgICAgICAgU3RyaW5nICAgQGRlZmF1bHQodXVpZCgpKQogIGVtYWlsICAgICBTdHJpbmcgICBAdW5pcXVlCiAgdXNlcm5hbWUgIFN0cmluZz8KICBwYXNzd29yZCAgU3RyaW5nCiAgZmlyc3ROYW1lIFN0cmluZz8gIEBtYXAoImZpcnN0bmFtZSIpCiAgbGFzdE5hbWUgIFN0cmluZz8gIEBtYXAoImxhc3RuYW1lIikKICBwaG9uZSAgICAgU3RyaW5nPwogIGlzQWN0aXZlICBCb29sZWFuICBAZGVmYXVsdCh0cnVlKSBAbWFwKCJhY3RpdmUiKQogIGlzRGVsZXRlZCBCb29sZWFuICBAZGVmYXVsdChmYWxzZSkgQG1hcCgiYWNjb3VudF9kZWxldGVkIikKICByb2xlICAgICAgUm9sZVR5cGUKCiAgQEBtYXAoImFkbWluaXN0cmF0b3JzIikKfQoKZW51bSBSb2xlVHlwZSB7CiAgc3VwZXJfYWRtaW4KICBjbGllbnRfbWFuYWdlcgogIG1hbmFnZV91c2VycwogIGFsbAogIG5ld19yb2xlCiAgYXBpX2NsaWVudAp9Cgptb2RlbCBSb2xlIHsKICBpZCAgICAgICAgU3RyaW5nICAgQGlkIEBkZWZhdWx0KGF1dG8oKSkgQG1hcCgiX2lkIikgQGRiLk9iamVjdElkCiAgY3JlYXRlZEF0IERhdGVUaW1lIEBkZWZhdWx0KG5vdygpKSBAbWFwKCJjcmVhdGVkX2F0IikKCiAgbmFtZSAgICAgIFN0cmluZyAgQHVuaXF1ZQogIGlzRGVsZXRlZCBCb29sZWFuIEBkZWZhdWx0KGZhbHNlKSBAbWFwKCJkZWxldGVkIikKCiAgQEBtYXAoInJvbGVzIikKfQo=",
-  "inlineSchemaHash": "41733c1a765c1e01049f5bc5f9e4251220afe8ff356033a09580b4020b828f1e",
-  "noEngine": false
-}
-config.dirname = '/'
+  inlineSchema:
+    'Ly8gVGhpcyBpcyB5b3VyIFByaXNtYSBzY2hlbWEgZmlsZSwKLy8gbGVhcm4gbW9yZSBhYm91dCBpdCBpbiB0aGUgZG9jczogaHR0cHM6Ly9wcmlzLmx5L2QvcHJpc21hLXNjaGVtYQoKZ2VuZXJhdG9yIGNsaWVudCB7CiAgcHJvdmlkZXIgPSAicHJpc21hLWNsaWVudC1qcyIKICBvdXRwdXQgICA9ICIuLi9saWJzL2NvbW1vbi9zcmMvcHJpc21hL2NsaWVudC1hdXRoIgp9CgpkYXRhc291cmNlIGRiIHsKICBwcm92aWRlciA9ICJtb25nb2RiIgogIHVybCAgICAgID0gZW52KCJBVVRIX0RBVEFCQVNFX1VSTCIpCn0KCm1vZGVsIFVzZXIgewogIGlkICAgICAgICBTdHJpbmcgICBAaWQgQGRlZmF1bHQoYXV0bygpKSBAbWFwKCJfaWQiKSBAZGIuT2JqZWN0SWQKICBjcmVhdGVkQXQgRGF0ZVRpbWUgQGRlZmF1bHQobm93KCkpIEBtYXAoImNyZWF0ZWRfYXQiKQogIHVwZGF0ZWRBdCBEYXRlVGltZSBAdXBkYXRlZEF0IEBtYXAoInVwZGF0ZWRfYXQiKQoKICB1aWQgICAgICAgU3RyaW5nICAgQGRlZmF1bHQodXVpZCgpKQogIGVtYWlsICAgICBTdHJpbmcgICBAdW5pcXVlCiAgdXNlcm5hbWUgIFN0cmluZz8KICBwYXNzd29yZCAgU3RyaW5nCiAgZmlyc3ROYW1lIFN0cmluZz8gIEBtYXAoImZpcnN0bmFtZSIpCiAgbGFzdE5hbWUgIFN0cmluZz8gIEBtYXAoImxhc3RuYW1lIikKICBwaG9uZSAgICAgU3RyaW5nPwogIGlzQWN0aXZlICBCb29sZWFuICBAZGVmYXVsdCh0cnVlKSBAbWFwKCJhY3RpdmUiKQogIGlzRGVsZXRlZCBCb29sZWFuICBAZGVmYXVsdChmYWxzZSkgQG1hcCgiYWNjb3VudF9kZWxldGVkIikKICByb2xlICAgICAgUm9sZVR5cGUKCiAgQEBtYXAoImFkbWluaXN0cmF0b3JzIikKfQoKZW51bSBSb2xlVHlwZSB7CiAgc3VwZXJfYWRtaW4KICBjbGllbnRfbWFuYWdlcgogIG1hbmFnZV91c2VycwogIGFsbAogIG5ld19yb2xlCiAgYXBpX2NsaWVudAp9Cgptb2RlbCBSb2xlIHsKICBpZCAgICAgICAgU3RyaW5nICAgQGlkIEBkZWZhdWx0KGF1dG8oKSkgQG1hcCgiX2lkIikgQGRiLk9iamVjdElkCiAgY3JlYXRlZEF0IERhdGVUaW1lIEBkZWZhdWx0KG5vdygpKSBAbWFwKCJjcmVhdGVkX2F0IikKCiAgbmFtZSAgICAgIFN0cmluZyAgQHVuaXF1ZQogIGlzRGVsZXRlZCBCb29sZWFuIEBkZWZhdWx0KGZhbHNlKSBAbWFwKCJkZWxldGVkIikKCiAgQEBtYXAoInJvbGVzIikKfQo=',
+  inlineSchemaHash:
+    '41733c1a765c1e01049f5bc5f9e4251220afe8ff356033a09580b4020b828f1e',
+  noEngine: false,
+};
+config.dirname = '/';
 
-config.runtimeDataModel = JSON.parse("{\"models\":{\"User\":{\"dbName\":\"administrators\",\"fields\":[{\"name\":\"id\",\"dbName\":\"_id\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":true,\"isReadOnly\":false,\"hasDefaultValue\":true,\"type\":\"String\",\"default\":{\"name\":\"auto\",\"args\":[]},\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"createdAt\",\"dbName\":\"created_at\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":true,\"type\":\"DateTime\",\"default\":{\"name\":\"now\",\"args\":[]},\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"updatedAt\",\"dbName\":\"updated_at\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"DateTime\",\"isGenerated\":false,\"isUpdatedAt\":true},{\"name\":\"uid\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":true,\"type\":\"String\",\"default\":{\"name\":\"uuid\",\"args\":[]},\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"email\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":true,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"username\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":false,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"password\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"firstName\",\"dbName\":\"firstname\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":false,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"lastName\",\"dbName\":\"lastname\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":false,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"phone\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":false,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"isActive\",\"dbName\":\"active\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":true,\"type\":\"Boolean\",\"default\":true,\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"isDeleted\",\"dbName\":\"account_deleted\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":true,\"type\":\"Boolean\",\"default\":false,\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"role\",\"kind\":\"enum\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"RoleType\",\"isGenerated\":false,\"isUpdatedAt\":false}],\"primaryKey\":null,\"uniqueFields\":[],\"uniqueIndexes\":[],\"isGenerated\":false},\"Role\":{\"dbName\":\"roles\",\"fields\":[{\"name\":\"id\",\"dbName\":\"_id\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":true,\"isReadOnly\":false,\"hasDefaultValue\":true,\"type\":\"String\",\"default\":{\"name\":\"auto\",\"args\":[]},\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"createdAt\",\"dbName\":\"created_at\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":true,\"type\":\"DateTime\",\"default\":{\"name\":\"now\",\"args\":[]},\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"name\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":true,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"isDeleted\",\"dbName\":\"deleted\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":true,\"type\":\"Boolean\",\"default\":false,\"isGenerated\":false,\"isUpdatedAt\":false}],\"primaryKey\":null,\"uniqueFields\":[],\"uniqueIndexes\":[],\"isGenerated\":false}},\"enums\":{\"RoleType\":{\"values\":[{\"name\":\"super_admin\",\"dbName\":null},{\"name\":\"client_manager\",\"dbName\":null},{\"name\":\"manage_users\",\"dbName\":null},{\"name\":\"all\",\"dbName\":null},{\"name\":\"new_role\",\"dbName\":null},{\"name\":\"api_client\",\"dbName\":null}],\"dbName\":null}},\"types\":{}}")
-defineDmmfProperty(exports.Prisma, config.runtimeDataModel)
-
+config.runtimeDataModel = JSON.parse(
+  '{"models":{"User":{"dbName":"administrators","fields":[{"name":"id","dbName":"_id","kind":"scalar","isList":false,"isRequired":true,"isUnique":false,"isId":true,"isReadOnly":false,"hasDefaultValue":true,"type":"String","default":{"name":"auto","args":[]},"isGenerated":false,"isUpdatedAt":false},{"name":"createdAt","dbName":"created_at","kind":"scalar","isList":false,"isRequired":true,"isUnique":false,"isId":false,"isReadOnly":false,"hasDefaultValue":true,"type":"DateTime","default":{"name":"now","args":[]},"isGenerated":false,"isUpdatedAt":false},{"name":"updatedAt","dbName":"updated_at","kind":"scalar","isList":false,"isRequired":true,"isUnique":false,"isId":false,"isReadOnly":false,"hasDefaultValue":false,"type":"DateTime","isGenerated":false,"isUpdatedAt":true},{"name":"uid","kind":"scalar","isList":false,"isRequired":true,"isUnique":false,"isId":false,"isReadOnly":false,"hasDefaultValue":true,"type":"String","default":{"name":"uuid","args":[]},"isGenerated":false,"isUpdatedAt":false},{"name":"email","kind":"scalar","isList":false,"isRequired":true,"isUnique":true,"isId":false,"isReadOnly":false,"hasDefaultValue":false,"type":"String","isGenerated":false,"isUpdatedAt":false},{"name":"username","kind":"scalar","isList":false,"isRequired":false,"isUnique":false,"isId":false,"isReadOnly":false,"hasDefaultValue":false,"type":"String","isGenerated":false,"isUpdatedAt":false},{"name":"password","kind":"scalar","isList":false,"isRequired":true,"isUnique":false,"isId":false,"isReadOnly":false,"hasDefaultValue":false,"type":"String","isGenerated":false,"isUpdatedAt":false},{"name":"firstName","dbName":"firstname","kind":"scalar","isList":false,"isRequired":false,"isUnique":false,"isId":false,"isReadOnly":false,"hasDefaultValue":false,"type":"String","isGenerated":false,"isUpdatedAt":false},{"name":"lastName","dbName":"lastname","kind":"scalar","isList":false,"isRequired":false,"isUnique":false,"isId":false,"isReadOnly":false,"hasDefaultValue":false,"type":"String","isGenerated":false,"isUpdatedAt":false},{"name":"phone","kind":"scalar","isList":false,"isRequired":false,"isUnique":false,"isId":false,"isReadOnly":false,"hasDefaultValue":false,"type":"String","isGenerated":false,"isUpdatedAt":false},{"name":"isActive","dbName":"active","kind":"scalar","isList":false,"isRequired":true,"isUnique":false,"isId":false,"isReadOnly":false,"hasDefaultValue":true,"type":"Boolean","default":true,"isGenerated":false,"isUpdatedAt":false},{"name":"isDeleted","dbName":"account_deleted","kind":"scalar","isList":false,"isRequired":true,"isUnique":false,"isId":false,"isReadOnly":false,"hasDefaultValue":true,"type":"Boolean","default":false,"isGenerated":false,"isUpdatedAt":false},{"name":"role","kind":"enum","isList":false,"isRequired":true,"isUnique":false,"isId":false,"isReadOnly":false,"hasDefaultValue":false,"type":"RoleType","isGenerated":false,"isUpdatedAt":false}],"primaryKey":null,"uniqueFields":[],"uniqueIndexes":[],"isGenerated":false},"Role":{"dbName":"roles","fields":[{"name":"id","dbName":"_id","kind":"scalar","isList":false,"isRequired":true,"isUnique":false,"isId":true,"isReadOnly":false,"hasDefaultValue":true,"type":"String","default":{"name":"auto","args":[]},"isGenerated":false,"isUpdatedAt":false},{"name":"createdAt","dbName":"created_at","kind":"scalar","isList":false,"isRequired":true,"isUnique":false,"isId":false,"isReadOnly":false,"hasDefaultValue":true,"type":"DateTime","default":{"name":"now","args":[]},"isGenerated":false,"isUpdatedAt":false},{"name":"name","kind":"scalar","isList":false,"isRequired":true,"isUnique":true,"isId":false,"isReadOnly":false,"hasDefaultValue":false,"type":"String","isGenerated":false,"isUpdatedAt":false},{"name":"isDeleted","dbName":"deleted","kind":"scalar","isList":false,"isRequired":true,"isUnique":false,"isId":false,"isReadOnly":false,"hasDefaultValue":true,"type":"Boolean","default":false,"isGenerated":false,"isUpdatedAt":false}],"primaryKey":null,"uniqueFields":[],"uniqueIndexes":[],"isGenerated":false}},"enums":{"RoleType":{"values":[{"name":"super_admin","dbName":null},{"name":"client_manager","dbName":null},{"name":"manage_users","dbName":null},{"name":"all","dbName":null},{"name":"new_role","dbName":null},{"name":"api_client","dbName":null}],"dbName":null}},"types":{}}',
+);
+defineDmmfProperty(exports.Prisma, config.runtimeDataModel);
 
 config.injectableEdgeEnv = () => ({
   parsed: {
-    AUTH_DATABASE_URL: typeof globalThis !== 'undefined' && globalThis['AUTH_DATABASE_URL'] || typeof process !== 'undefined' && process.env && process.env.AUTH_DATABASE_URL || undefined
-  }
-})
+    AUTH_DATABASE_URL:
+      (typeof globalThis !== 'undefined' && globalThis['AUTH_DATABASE_URL']) ||
+      (typeof process !== 'undefined' &&
+        process.env &&
+        process.env.AUTH_DATABASE_URL) ||
+      undefined,
+  },
+});
 
-if (typeof globalThis !== 'undefined' && globalThis['DEBUG'] || typeof process !== 'undefined' && process.env && process.env.DEBUG || undefined) {
-  Debug.enable(typeof globalThis !== 'undefined' && globalThis['DEBUG'] || typeof process !== 'undefined' && process.env && process.env.DEBUG || undefined)
+if (
+  (typeof globalThis !== 'undefined' && globalThis['DEBUG']) ||
+  (typeof process !== 'undefined' && process.env && process.env.DEBUG) ||
+  undefined
+) {
+  Debug.enable(
+    (typeof globalThis !== 'undefined' && globalThis['DEBUG']) ||
+      (typeof process !== 'undefined' && process.env && process.env.DEBUG) ||
+      undefined,
+  );
 }
 
-const PrismaClient = getPrismaClient(config)
-exports.PrismaClient = PrismaClient
-Object.assign(exports, Prisma)
-
+const PrismaClient = getPrismaClient(config);
+exports.PrismaClient = PrismaClient;
+Object.assign(exports, Prisma);
